42415eaecd0953dbc93986785ac6c3f6b65c2961
Yuki <workcomputer.com>
2023-10-11 13:51:37 +0800

    train voc dataset

---
+++ configs/yolov8/yolov8_n_syncbn_fast_8xb16-100e_voc0712.py
@@ -0,0 +1,11 @@
+_base_ = './yolov8_s_syncbn_fast_8xb16-100e_voc0712.py'
+
+load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov8/yolov8_n_syncbn_fast_8xb16-500e_coco/yolov8_n_syncbn_fast_8xb16-500e_coco_20230114_131804-88c11cdb.pth'  # noqa
+
+deepen_factor = 0.33
+widen_factor = 0.25
+
+model = dict(
+    backbone=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    neck=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    bbox_head=dict(head_module=dict(widen_factor=widen_factor)))
---
+++ configs/yolov8/yolov8_x_syncbn_fast_8xb16-100e_voc0712.py
@@ -0,0 +1,12 @@
+_base_ = './yolov8_s_syncbn_fast_8xb16-100e_voc0712.py'
+
+load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov8/yolov8_x_syncbn_fast_8xb16-500e_coco/yolov8_x_syncbn_fast_8xb16-500e_coco_20230218_023338-5674673c.pth'  # noqa
+
+deepen_factor = 1.00
+widen_factor = 1.25
+
+model = dict(
+    backbone=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    neck=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    bbox_head=dict(head_module=dict(widen_factor=widen_factor)))
+
